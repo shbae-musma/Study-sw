dbae513cbd663cb7d551523cafd9138b6ae4a956
배석현 <shb@musma.net>
2022-06-13 16:38:02 +0900

    chore: add jira issue test
    SWTEAM-253

--- flutter2/flutter_book_list/lib/main.dart
+++ flutter2/flutter_book_list/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Drama list 앱',
+      title: 'Drama 리스트 앱',
       home: ListScreen(),
     );
   }
